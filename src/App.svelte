4fc5796f22fc4195f3003ea722e4bef417591101
knockoffduck <ddarm4@gmail.com>
2022-08-22 00:17:29 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+	import AddExerciseForm from "./AddExerciseForm.svelte";
+
+	const exercises = [];
+
+	function addExercise(exercise) {
+		exercises.push({
+			title: exercise,
+			completed: false,
+		});
+
+		exercises = exercises;
+	}
+</script>
+
+<main>
+	<AddExerciseForm {addExercise} />
+	<ul>
+		{#each exercises as { title, completed }, i}
+			<li>
+				{title}
+			</li>
+		{/each}
+	</ul>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
